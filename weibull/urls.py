479790379a7edbcbd3d1a2d3b653baa7e95fc68c
Erick Navarro <erickxls@gmail.com>
2013-01-12 20:14:45 -0500

    added home view

--- weibull/urls.py
+++ weibull/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'weibull.views.home', name='home'),
+    url(r'^$', 'weibull.views.home', name='home'),
     # url(r'^weibull/', include('weibull.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
