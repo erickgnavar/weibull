f60728f5c96769d0fd0d7e764acf6b99bb79277b
Erick Navarro <erickxls@gmail.com>
2013-01-13 20:02:18 -0500

    added templates and updated settings with CDN url

--- weibull/settings.py
+++ weibull/settings.py
@@ -67,7 +67,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://erickxls.appspot.com/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
--- weibull/views.py
+++ weibull/views.py
@@ -1,4 +1,26 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 
 def home(request):
-    return render_to_response('home.html')
+    context = {
+        'data': weibull(2, 1, initial=2, repetition=51)
+    }
+    return render_to_response('home.html', context, RequestContext(request))
+
+
+def weibull(alpha, beta, increment=0.05, initial=0, repetition=100):
+    import math
+    data = []
+    i = 0
+    x = initial
+    while i < repetition:
+        r = -1 * math.pow(x / beta, alpha)
+        result = 1 - math.pow(math.e, r)
+        data.append({
+            'x': x,
+            'y': result
+        })
+        x += increment
+        i += 1
+    return data
